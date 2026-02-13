c056d1fba132ef2a2be6fdbc1734536c72df1e12
Sudeep  B N <bnsudeep11@gmail.com>
2026-02-13 22:36:16 +0530

    Add files via upload

---
+++ Divisiable.java
@@ -0,0 +1,19 @@
+public class Divisiable {
+
+    public static int  Divisiable(int number, int divisiable){
+
+            return number % divisiable;
+         }
+
+ public static void main(String [] args){
+
+  int number = 13;
+
+  int divisiable = 4;
+
+  int result = Divisiable(number,divisiable);
+
+  System.out.println(result);
+
+}
+}
