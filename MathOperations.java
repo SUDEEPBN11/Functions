6ac404823660a1ccb12c0305438a2e0c02a1fa38
Sudeep  B N <bnsudeep11@gmail.com>
2026-02-13 22:37:53 +0530

    Add files via upload

---
+++ MathOperations.java
@@ -0,0 +1,43 @@
+public class MathOperations {
+
+    public static void main(String[] args){
+
+        double x = Math.floor(3.5);
+        
+        double y = Math.ceil(5.4);
+
+        double z = Math.round(5.2);
+
+        double s = Math.round(5.8);
+;
+        double log = Math.E;
+
+        System.out.println(log);
+
+        double log10 = Math.log(1);
+
+        double squre = Math.sqrt(4);
+
+        System.out.println("Sqare of number = "+squre);
+
+        double power = Math.pow(2,2);
+        
+        System.out.println("Power of number ="+power);
+
+        System.out.println(log10);
+
+        System.out.println(x);
+
+        System.out.println(y);
+
+        System.out.println(z);
+
+        System.out.println(s);
+
+
+
+
+
+    }
+    
+}
