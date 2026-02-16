847be0930c05f0196b7a6fecabbbb31d80663ec4
Sudeep  B N <bnsudeep11@gmail.com>
2026-02-16 15:40:07 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+   private static name;
+
+   private  static designation;
+   
+   private static empID;
+
+   private static basic salary ;
+
+
+   public string read(){}
+
+   public void write(){}
+
+public static void main(String[] args){}
+    
+}
